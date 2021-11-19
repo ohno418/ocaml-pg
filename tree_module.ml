dc51663c3ae4a2ab1ec22689ec1d43a4137ad20f
taro0 <yutaro.ono.418@gmail.com>
2021-11-19 12:22:10 +0900

    Tree moudle and signature

---
+++ tree_module.ml
@@ -0,0 +1,42 @@
+module Tree = struct
+  (* binary search tree type *)
+  type ('a, 'b) t = Empty
+                  | Node of ('a, 'b) t * 'a * 'b * ('a, 'b) t
+
+  let empty = Empty
+
+  let rec insert tree k v = match tree with
+      Empty -> Node (Empty, k, v, Empty)
+    | Node (left, key, value, right) ->
+        if k = key then Node (left, key, v, right)
+        else if k < key
+             then Node (insert left k v, key, value, right)
+             else Node (left, key, value, insert right k v)
+
+  let rec search tree k = match tree with
+      Empty -> raise Not_found
+    | Node (left, key, value, right) ->
+        if k = key then value
+        else if k < key then search left k
+                        else search right k
+end
+
+module type Tree_t = sig
+  type ('a, 'b) t
+    (* Tree type which has key ('a) and value (b). *)
+
+  val empty : ('a, 'b) t
+    (* An empty tree. *)
+
+  val insert : ('a, 'b) t -> 'a -> 'b -> ('a, 'b) t
+    (* usage: insert tree key value *)
+    (* Insert key and value into tree, and return it.
+       If key already exists, overwrite the value. *)
+
+  val search : ('a, 'b) t -> 'a -> 'b
+    (* usage: search tree key *)
+    (* Search value for the key in the tree.
+       Raise Not_found if not found the key. *)
+end
+
+module NewTree = (Tree : Tree_t) ;;
